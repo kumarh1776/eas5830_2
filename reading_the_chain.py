aabf034449b7dc1e4298c7a4cd8199acfaa2b885
kumarh1776 <59850401+kumarh1776@users.noreply.github.com>
2024-07-07 17:07:22 -0400

    Add files via upload

--- reading_the_chain.py
+++ reading_the_chain.py
@@ -69,8 +69,12 @@ def is_ordered_block(w3, block_num):
 			return tx['gasPrice'] - base_fee_per_gas
 	
 	transactions = block['transactions']
-	priority_fees = [check_priority_fee(tx) for tx in transactions]
+	priority_fees = []
 
+  for tx_hash in transactions:
+    tx = w3.eth.get_transaction(tx_hash)
+    priority_fees.append(check_priority_fee(tx))
+    
 	ordered = all(priority_fees[i] >= priority_fees[i+1] for i in range(len(priority_fees) - 1))
 
 	return ordered
